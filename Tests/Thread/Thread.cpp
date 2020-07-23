3c61d67adbad9ed8197bab0ac53009fe5aa0edae
Kacper Słomiński <kacper.slominski72@gmail.com>
2020-07-23 04:15:10 +0200

    Implement hsd::thread

---
+++ Tests/Thread/Thread.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../../cpp/Thread/Thread.hpp"
+
+void foo(int a, float b) {
+	std::cout << a << " " << b << std::endl;
+}
+
+int main() {
+	hsd::thread t{foo, 1, 2.3f};
+	t.join();
+}

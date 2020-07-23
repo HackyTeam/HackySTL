f7daf48f4cb32d1ab87448afdfd37827a6a9a70a
CatalinNic1 <catalinnicolau2013@gmail.com>
2020-07-23 10:49:44 +0300

    rewrite hsd::type_info::strcmp

--- cpp/Any/Any.hpp
+++ cpp/Any/Any.hpp
@@ -28,34 +28,33 @@ namespace hsd
 
         constexpr int strcmp(const char* lhs, const char* rhs)
         {
-            int i = 0;
+            size_t i = 0;
 
-	    while(lhs[i] && rhs[i])
-	    {
-	       	 if(lhs[i] < rhs[i])
-	         {
-                     return -1;
-                 }
-	         else if(lhs[i] > rhs[i])
-	         {
-                     return 1;
-                 }
+	        while(lhs[i] && rhs[i])
+	        {
+	           	if(lhs[i] < rhs[i])
+	            {
+                    return -1;
+                }
+	            else if(lhs[i] > rhs[i])
+	            {
+                    return 1;
+                }
+	            i++;
+            } 
 
-	         i++;
-	     }
-
-	     if(lhs[i] < rhs[i])
-	     {
-                 return -1;
-             }
-	     else if(lhs[i] > rhs[i])
-	     {
-                 return 1;
-             }
-	     else
-	     {
-                 return 0;
-             }
+	        if(lhs[i] < rhs[i])
+	        {
+                return -1;
+            }
+	        else if(lhs[i] > rhs[i])
+	        {
+                return 1;
+            }
+	        else
+	        {
+                return 0;
+            }
         }
     public:
         type_info() = delete;

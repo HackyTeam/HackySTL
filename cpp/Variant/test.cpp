b2569076b2afe363d6ed3201e556a0c6b9b0c060
dekrain <dawidkrainski8@gmail.com>
2020-07-25 22:29:09 +0200

    Variant full program :tada: :tada:

--- cpp/Variant/test.cpp
+++ cpp/Variant/test.cpp
@@ -4,8 +4,12 @@
 
 struct A {};
 struct B {
-    B(int) {}
+    int a;
+    constexpr B(int a) : a(a) {}
 };
+
+constexpr bool operator==(A, A) noexcept {return true; }
+constexpr bool operator==(B a, B b) noexcept {return a.a == b.a; }
 
 int main() {
     hsd::variant<int, float> va1(5);
@@ -22,11 +26,30 @@ int main() {
     std::cout << va3.index() << std::endl;
     std::cout << va4.index() << std::endl;
 
-    std::cout << hsd::get<0>(va1) << std::endl;
-    try {
+    std::cout << va1.get<0>() << std::endl;
+    /*try {
         // This should throw -vvv
-        std::cout << hsd::get<float>(va1) << std::endl;
+        std::cout << va1.get<float>() << std::endl;
     } catch (hsd::bad_variant_access& err) {
         std::cerr << "Caught error: " << err.what() << std::endl;
+    }*/
+    if (auto* val = hsd::get_if<float>(&va1)) {
+        std::cout << "Got value: " << *val << std::endl;
+    } else {
+        std::cout << "No value of type float in va1" << std::endl;
     }
+    if (auto* val = va2.get_if<B>()) {
+        std::cout << "B inside va2: " << val->a << std::endl;
+    } else {
+        std::cout << "No B in va2" << std::endl;
+    }
+
+    std::cout << std::boolalpha;
+    std::cout << "va2 == va3: " << (va2 == va3) << std::endl;
+    va2 = va3;
+    std::cout << "(after assignment) " << (va2 == va3) << std::endl;
+    va3 = hsd::variant<A, B>(6);
+    std::cout << "(after new assignment)" << std::endl << "va3 == va4: " << (va3 == va4) << std::endl;
+    va3.get<B>().a = 5;
+    std::cout << "(after modification) " << (va3 == va4) << std::endl;
 }
--- cpp/Variant/variant.hpp
+++ cpp/Variant/variant.hpp
@@ -65,20 +65,33 @@ struct variant<_Tfirst, _Trest...> {
             _Value(std::forward<_Tother>(val), _variant_index<typename _type_for<_Tother>::type, _Tfirst, _Trest...>()) {}
 
     ~variant() {
-        _Value.destroy(_StoredIndex);
+        _destroy();
     }
 
-    constexpr variant& operator=(const variant& rhs);
-    constexpr variant& operator=(variant&& rhs);
+    constexpr variant& operator=(const variant& rhs) {
+        _StorageTraits::assign_copy(_Value, rhs._Value, _StoredIndex, rhs.index());
+        return *this;
+    }
+    constexpr variant& operator=(variant&& rhs) {
+        _StorageTraits::assign_move(_Value, rhs._Value, _StoredIndex, rhs.index());
+        return *this;
+    }
 
-    template <typename _Tother>
-    constexpr variant& operator=(_Tother&& rhs);
+    template <typename _Tother, typename = std::enable_if_t<!std::is_same_v<variant, std::decay_t<_Tother>>>>
+    constexpr variant& operator=(_Tother&& rhs) {
+        _StorageTraits::assign_fwd<_variant_index<typename _type_for<_Tother>::type, _Tfirst, _Trest...>::value>(_Value, _StoredIndex, std::forward<_Tother>(rhs));
+        return *this;
+    }
 
-    constexpr size_t index() const {
+    constexpr size_t index() const noexcept {
         return _StoredIndex;
     }
 
 private:
+    void _destroy() {
+        _storage_traits<decltype(_Value)>::destroy(_Value, _StoredIndex);
+    }
+
     template <typename... _Ts>
     struct _select_overload_for {
         constexpr static void get();
@@ -103,13 +116,15 @@ private:
     struct _tag_first_member {};
     struct _tag_rest_member {};
 
+    template <typename _Stor>
+    struct _storage_traits;
+
     template <typename... _Ts>
     struct _variant_storage {
         constexpr _variant_storage(const _variant_storage&, size_t) {}
         constexpr _variant_storage(_variant_storage&&, size_t) {}
         template <typename _Ty>
         constexpr _variant_storage(_Ty&&, size_t) {}
-        constexpr void destroy(size_t) {}
     };
 
     template <typename _Tfirst, typename... _Trest>
@@ -118,6 +133,8 @@ private:
             _Tfirst _value_first;
             _variant_storage<_Trest...> _storage_rest;
         };
+
+        using _RestTraits = _storage_traits<_variant_storage<_Trest...>>;
 
         // Default constructor
         constexpr _variant_storage() : _value_first() {}
@@ -160,30 +177,179 @@ private:
             else
                 return _variant_storage(_tag_rest_member(), forward<_Ty>(val), std::integral_constant<size_t, _Idx-1>());
         }
-        
+    };
+
+
+    template <typename... _Ts>
+    struct _storage_traits<_variant_storage<_Ts...>> {
+        using Storage = _variant_storage<_Ts...>;
+        static constexpr void destroy(Storage&, size_t) {}
+        constexpr static bool equals_impl(size_t, Storage const&, Storage const&) {
+            return false;
+        }
+        constexpr static void assign_copy_impl(Storage&, Storage const&, size_t) {}
+        constexpr static void assign_move_impl(Storage&, Storage&, size_t) {}
+    };
+
+    template <typename _Tfirst, typename... _Trest>
+    struct _storage_traits<_variant_storage<_Tfirst, _Trest...>> {
+        using Storage = _variant_storage<_Tfirst, _Trest...>;
+        // Out-of-place constructor
+        constexpr static void construct_idx(Storage& l, Storage const& r, size_t id) {
+            new (&l) Storage(r, id);
+        }
+        constexpr static void construct_idx(Storage& l, Storage&& r, size_t id) {
+            new (&l) Storage(std::move(r), id);
+        }
+        template <size_t _Idx, typename _Ty>
+        constexpr static void construct_fwd(Storage& l, _Ty&& value) {
+            new (&l) Storage(std::forward<_Ty>(value), std::integral_constant<size_t, _Idx>());
+        }
         // Destructor
-        void destroy(size_t id) {
+        static void destroy(Storage& s, size_t id) {
             if (id == 0) {
-                _value_first.~_Tfirst();
+                s._value_first.~_Tfirst();
             } else {
-                _storage_rest.destroy(id-1);
+                Storage::_RestTraits::destroy(s._storage_rest, id-1);
+            }
+        }
+        template <size_t _TheIdx>
+        constexpr static auto& get_impl(Storage const& storage) {
+            if constexpr (_TheIdx == 0) {
+                return storage._value_first;
+            } else {
+                return Storage::_RestTraits::template get_impl<_TheIdx-1>(storage._storage_rest);
+            }
+        }
+        constexpr static bool equals_impl(size_t id, Storage const& a, Storage const& b) {
+            if (id == 0) {
+                return a._value_first == b._value_first;
+            } else {
+                return Storage::_RestTraits::equals_impl(id-1, a._storage_rest, b._storage_rest);
+            }
+        }
+        constexpr static void assign_copy_impl(Storage& l, Storage const& r, size_t id) {
+            if (id == 0) {
+                l._value_first = r._value_first;
+            } else {
+                Storage::_RestTraits::assign_copy_impl(l._storage_rest, r._storage_rest, id-1);
+            }
+        }
+        constexpr static void assign_move_impl(Storage& l, Storage& r, size_t id) {
+            if (id == 0) {
+                l._value_first = std::move(r._value_first);
+            } else {
+                Storage::_RestTraits::assign_move_impl(l._storage_rest, r._storage_rest, id-1);
+            }
+        }
+        constexpr static void assign_copy(Storage& l, Storage const& r, size_t& il, size_t ir) {
+            if (il != ir) {
+                destroy(l, il);
+                il = ir;
+                construct_idx(l, r, ir);
+            }
+            assign_copy_impl(l, r, ir);
+        }
+        constexpr static void assign_move(Storage& l, Storage& r, size_t& il, size_t ir) {
+            if (il != ir) {
+                destroy(l, il);
+                il = ir;
+                construct_idx(l, std::move(r), ir);
+            }
+            assign_move_impl(l, r, ir);
+        }
+        template <size_t _Idx, typename _Ty>
+        constexpr static void assign_fwd_current(Storage& l, _Ty&& value) {
+            if constexpr (_Idx == 0) {
+                l._value_first = std::forward<_Ty>(value);
+            } else {
+                Storage::_RestTraits::assign_fwd_current(l._storage_rest, std::forward<_Ty>(value));
+            }
+        }
+        template <size_t _Idx, typename _Ty>
+        constexpr static void assign_fwd(Storage& l, size_t& il, _Ty&& value) {
+            if (_Idx == il) {
+                assign_fwd_current<_Idx>(l, std::forward<_Ty>(value));
+            } else {
+                destroy(l, il);
+                il = _Idx;
+                construct_fwd<_Idx>(l, std::forward<_Ty>(value));
             }
         }
     };
 
     _variant_storage<_Tfirst, _Trest...> _Value;
+    using _StorageTraits = _storage_traits<_variant_storage<_Tfirst, _Trest...>>;
 public:
-    template <size_t _Idx, typename... _Ts>
-    friend constexpr variant_alternative_t<_Idx, variant<_Ts...>>& get(variant<_Ts...>& v);
+    template <size_t _Idx>
+    constexpr variant_alternative_t<_Idx, variant>& get() {
+        return const_cast<variant_alternative_t<_Idx, variant>&>(const_cast<variant const*>(this)->get<_Idx>());
+    }
 
-    template <size_t _Idx, typename... _Ts>
-    friend constexpr variant_alternative_t<_Idx, variant<_Ts...>> const& get(variant<_Ts...> const& v);
+    template <size_t _Idx>
+    constexpr variant_alternative_t<_Idx, variant> const& get() const {
+        if (index() != _Idx)
+            throw bad_variant_access();
+        return _StorageTraits::template get_impl<_Idx>(_Value);
+    }
 
-    template <typename _Ty, typename... _Ts>
-    friend constexpr _Ty& get(variant<_Ts...>& v);
+    template <typename _Ty>
+    constexpr _Ty& get() {
+        return const_cast<_Ty&>(const_cast<variant const*>(this)->get<_Ty>());
+    }
 
-    template <typename _Ty, typename... _Ts>
-    friend constexpr _Ty const& get(variant<_Ts...> const& v);
+    template <typename _Ty>
+    constexpr _Ty const& get() const {
+        return get<_variant_index<_Ty, _Tfirst, _Trest...>::value>();
+    }
+
+
+    template <size_t _Idx>
+    constexpr variant_alternative_t<_Idx, variant>* get_if() noexcept {
+        return const_cast<variant_alternative_t<_Idx, variant>*>(const_cast<variant const*>(this)->get_if<_Idx>());
+    }
+
+    template <size_t _Idx>
+    constexpr variant_alternative_t<_Idx, variant> const* get_if() const noexcept {
+        if (index() != _Idx)
+            return nullptr;
+        return &_StorageTraits::template get_impl<_Idx>(_Value);
+    }
+
+    template <typename _Ty>
+    constexpr _Ty* get_if() noexcept {
+        return const_cast<_Ty*>(const_cast<variant const*>(this)->get_if<_Ty>());
+    }
+
+    template <typename _Ty>
+    constexpr _Ty const* get_if() const noexcept {
+        return get_if<_variant_index<_Ty, _Tfirst, _Trest...>::value>();
+    }
+
+
+    // requires _Ty is a variant alternative
+    template <typename _Ty>
+    constexpr _Ty& emplace_one(_Ty&& value) {
+        _destroy();
+        _StoredIndex = _variant_index<std::remove_cv_t<std::remove_reference_t<_Ty>>, _Tfirst, _Trest...>::value;
+        _StorageTraits::construct_fwd<_variant_index<std::remove_cv_t<std::remove_reference_t<_Ty>>, _Tfirst, _Trest...>::value>(_Value, std::forward<_Ty>(value));
+    }
+
+
+    template <typename _Ty>
+    constexpr bool holds_alternative() const noexcept {
+        return index() == _variant_index<_Ty, _Tfirst, _Trest...>::value;
+    }
+
+    friend constexpr bool operator==(variant const& a, variant const& b) {
+        if (a.index() != b.index())
+            return false;
+        return _StorageTraits::equals_impl(a.index(), a._Value, b._Value);
+    }
+
+    friend constexpr bool operator!=(variant const& a, variant const& b) {
+        return !(a == b);
+    }
 };
 
 // class template variant_size
@@ -204,40 +370,55 @@ struct variant_alternative<_Idx, variant<_Ts...>> : _variant_alternative_helper<
 
 //// Utility functions
 // function template get
-
-// struct because lamda doesn't work :(  (maybe until C++20)
-// ALSO NICE CPEEPEE CAN'T GET TEMPLATES INSIDE LOCAL CLASS
-struct _get_worker {
-    template <size_t _TheIdx, typename _Stor>
-    constexpr static auto& doget(_Stor& storage) {
-        if constexpr (_TheIdx == 0) {
-            return storage._value_first;
-        } else {
-            return doget<_TheIdx-1>(storage._storage_rest);
-        }
-    }
-};
-
 template <size_t _Idx, typename... _Ts>
 constexpr variant_alternative_t<_Idx, variant<_Ts...>>& get(variant<_Ts...>& v) {
-    if (v.index() != _Idx)
-        throw bad_variant_access();
-    return _get_worker::template doget<_Idx>(v._Value);
+    return v.template get<_Idx>();
 }
 
 template <size_t _Idx, typename... _Ts>
 constexpr variant_alternative_t<_Idx, variant<_Ts...>> const& get(variant<_Ts...> const& v) {
-    return const_cast<variant_alternative_t<_Idx, variant<_Ts...>> const&>(get<_Idx>(const_cast<variant<_Ts...>&>(v)));
+    return v.template get<_Idx>();
 }
 
 template <typename _Ty, typename... _Ts>
 constexpr _Ty& get(variant<_Ts...>& v) {
-    return get<_variant_index<_Ty, _Ts...>::value>(v);
+    return v.template get<_Ty>();
 }
 
 template <typename _Ty, typename... _Ts>
 constexpr _Ty const& get(variant<_Ts...> const& v) {
-    return const_cast<_Ty const&>(get<_Ty>(const_cast<variant<_Ts...>&>(v)));
+    return v.template get<_Ty>();
+}
+
+// function template get_if
+template <size_t _Idx, typename... _Ts>
+constexpr variant_alternative_t<_Idx, variant<_Ts...>>* get_if(variant<_Ts...>* v) noexcept {
+    if (v == nullptr) return nullptr;
+    return v->template get_if<_Idx>();
+}
+
+template <size_t _Idx, typename... _Ts>
+constexpr variant_alternative_t<_Idx, variant<_Ts...>> const* get_if(variant<_Ts...> const* v) noexcept {
+    if (v == nullptr) return nullptr;
+    return v->template get_if<_Idx>();
+}
+
+template <typename _Ty, typename... _Ts>
+constexpr _Ty* get_if(variant<_Ts...>* v) noexcept {
+    if (v == nullptr) return nullptr;
+    return v->template get_if<_Ty>();
+}
+
+template <typename _Ty, typename... _Ts>
+constexpr _Ty const* get_if(variant<_Ts...> const* v) noexcept {
+    if (v == nullptr) return nullptr;
+    return v->template get_if<_Ty>();
+}
+
+// function template holds_alternative
+template <typename _Ty, typename... _Ts>
+constexpr bool holds_alternative(variant<_Ts...> const& v) noexcept {
+    return v.template holds_alternative<_Ty>();
 }
 
 } // end namespace hsd

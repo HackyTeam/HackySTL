08b21faabb46aff0712734a1a0789ae562ef4cfd
dekrain <dawidkrainski8@gmail.com>
2020-07-25 23:27:47 +0200

    Fix shadowing issues

--- cpp/Variant/variant.hpp
+++ cpp/Variant/variant.hpp
@@ -50,6 +50,171 @@ struct variant {
     static_assert(sizeof...(_Ts) != 0, "empty variant is not allowed");
 };
 
+struct _variant_tag_first_member {};
+struct _variant_tag_rest_member {};
+
+template <typename _Stor>
+struct _variant_storage_traits;
+
+template <typename... _Ts>
+struct _variant_storage {
+    constexpr _variant_storage(const _variant_storage&, size_t) {}
+    constexpr _variant_storage(_variant_storage&&, size_t) {}
+    template <typename _Ty>
+    constexpr _variant_storage(_Ty&&, size_t) {}
+};
+
+template <typename _Tfirst, typename... _Trest>
+struct _variant_storage<_Tfirst, _Trest...> {
+    union {
+        _Tfirst _value_first;
+        _variant_storage<_Trest...> _storage_rest;
+    };
+
+    using _RestTraits = _variant_storage_traits<_variant_storage<_Trest...>>;
+
+    // Default constructor
+    constexpr _variant_storage() : _value_first() {}
+
+    // Copy constructors
+    constexpr _variant_storage(_variant_tag_first_member, const _variant_storage& o)
+        : _value_first(o._value_first) {}
+    constexpr _variant_storage(_variant_tag_rest_member, const _variant_storage& o, size_t id)
+        : _storage_rest(o._storage_rest, id) {}
+    constexpr _variant_storage(const _variant_storage& o, size_t id)
+        : _variant_storage((id == 0)
+            ? _variant_storage(_variant_tag_first_member(), o)
+            : _variant_storage(_variant_tag_rest_member(), o, id-1)) {}
+
+    // Move constructors
+    constexpr _variant_storage(_variant_tag_first_member, _variant_storage&& o)
+        : _value_first(move(o._value_first)) {}
+    constexpr _variant_storage(_variant_tag_rest_member, _variant_storage&& o, size_t id)
+        : _storage_rest(move(o._storage_rest), id) {}
+    constexpr _variant_storage(_variant_storage&& o, size_t id)
+        : _variant_storage((id == 0)
+            ? _variant_storage(_variant_tag_first_member(), move(o))
+            : _variant_storage(_variant_tag_rest_member(), move(o), id-1)) {}
+    
+    // Forwarding constructors
+    template <typename _Ty>
+    constexpr _variant_storage(_variant_tag_first_member, _Ty&& val)
+        : _value_first(forward<_Ty>(val)) {}
+    template <typename _Ty, size_t _Idx>
+    constexpr _variant_storage(_variant_tag_rest_member, _Ty&& val, std::integral_constant<size_t, _Idx> id)
+        : _storage_rest(forward<_Ty>(val), id) {}
+    template <typename _Ty, size_t _Idx>
+    constexpr _variant_storage(_Ty&& val, std::integral_constant<size_t, _Idx> id)
+        : _variant_storage(this->_construct_index(forward<_Ty>(val), id)) {}
+    
+    template <typename _Ty, size_t _Idx>
+    constexpr static _variant_storage _construct_index(_Ty&& val, std::integral_constant<size_t, _Idx>) {
+        if constexpr (_Idx == 0)
+            return _variant_storage(_variant_tag_first_member(), forward<_Ty>(val));
+        else
+            return _variant_storage(_variant_tag_rest_member(), forward<_Ty>(val), std::integral_constant<size_t, _Idx-1>());
+    }
+};
+
+
+template <typename... _Ts>
+struct _variant_storage_traits<_variant_storage<_Ts...>> {
+    using Storage = _variant_storage<_Ts...>;
+    static constexpr void destroy(Storage&, size_t) {}
+    constexpr static bool equals_impl(size_t, Storage const&, Storage const&) {
+        return false;
+    }
+    constexpr static void assign_copy_impl(Storage&, Storage const&, size_t) {}
+    constexpr static void assign_move_impl(Storage&, Storage&, size_t) {}
+};
+
+template <typename _Tfirst, typename... _Trest>
+struct _variant_storage_traits<_variant_storage<_Tfirst, _Trest...>> {
+    using Storage = _variant_storage<_Tfirst, _Trest...>;
+    // Out-of-place constructor
+    constexpr static void construct_idx(Storage& l, Storage const& r, size_t id) {
+        new (&l) Storage(r, id);
+    }
+    constexpr static void construct_idx(Storage& l, Storage&& r, size_t id) {
+        new (&l) Storage(std::move(r), id);
+    }
+    template <size_t _Idx, typename _Ty>
+    constexpr static void construct_fwd(Storage& l, _Ty&& value) {
+        new (&l) Storage(std::forward<_Ty>(value), std::integral_constant<size_t, _Idx>());
+    }
+    // Destructor
+    static void destroy(Storage& s, size_t id) {
+        if (id == 0) {
+            s._value_first.~_Tfirst();
+        } else {
+            Storage::_RestTraits::destroy(s._storage_rest, id-1);
+        }
+    }
+    template <size_t _TheIdx>
+    constexpr static auto& get_impl(Storage const& storage) {
+        if constexpr (_TheIdx == 0) {
+            return storage._value_first;
+        } else {
+            return Storage::_RestTraits::template get_impl<_TheIdx-1>(storage._storage_rest);
+        }
+    }
+    constexpr static bool equals_impl(size_t id, Storage const& a, Storage const& b) {
+        if (id == 0) {
+            return a._value_first == b._value_first;
+        } else {
+            return Storage::_RestTraits::equals_impl(id-1, a._storage_rest, b._storage_rest);
+        }
+    }
+    constexpr static void assign_copy_impl(Storage& l, Storage const& r, size_t id) {
+        if (id == 0) {
+            l._value_first = r._value_first;
+        } else {
+            Storage::_RestTraits::assign_copy_impl(l._storage_rest, r._storage_rest, id-1);
+        }
+    }
+    constexpr static void assign_move_impl(Storage& l, Storage& r, size_t id) {
+        if (id == 0) {
+            l._value_first = std::move(r._value_first);
+        } else {
+            Storage::_RestTraits::assign_move_impl(l._storage_rest, r._storage_rest, id-1);
+        }
+    }
+    constexpr static void assign_copy(Storage& l, Storage const& r, size_t& il, size_t ir) {
+        if (il != ir) {
+            destroy(l, il);
+            il = ir;
+            construct_idx(l, r, ir);
+        }
+        assign_copy_impl(l, r, ir);
+    }
+    constexpr static void assign_move(Storage& l, Storage& r, size_t& il, size_t ir) {
+        if (il != ir) {
+            destroy(l, il);
+            il = ir;
+            construct_idx(l, std::move(r), ir);
+        }
+        assign_move_impl(l, r, ir);
+    }
+    template <size_t _Idx, typename _Ty>
+    constexpr static void assign_fwd_current(Storage& l, _Ty&& value) {
+        if constexpr (_Idx == 0) {
+            l._value_first = std::forward<_Ty>(value);
+        } else {
+            Storage::_RestTraits::assign_fwd_current(l._storage_rest, std::forward<_Ty>(value));
+        }
+    }
+    template <size_t _Idx, typename _Ty>
+    constexpr static void assign_fwd(Storage& l, size_t& il, _Ty&& value) {
+        if (_Idx == il) {
+            assign_fwd_current<_Idx>(l, std::forward<_Ty>(value));
+        } else {
+            destroy(l, il);
+            il = _Idx;
+            construct_fwd<_Idx>(l, std::forward<_Ty>(value));
+        }
+    }
+};
+
 template <typename _Tfirst, typename... _Trest>
 struct variant<_Tfirst, _Trest...> {
     constexpr variant() : _StoredIndex(0) {}
@@ -89,7 +254,7 @@ struct variant<_Tfirst, _Trest...> {
 
 private:
     void _destroy() {
-        _storage_traits<decltype(_Value)>::destroy(_Value, _StoredIndex);
+        _StorageTraits::destroy(_Value, _StoredIndex);
     }
 
     template <typename... _Ts>
@@ -97,10 +262,10 @@ private:
         constexpr static void get();
     };
 
-    template <typename _Tfirst, typename... _Trest>
-    struct _select_overload_for<_Tfirst, _Trest...> : _select_overload_for<_Trest...> {
-        using _select_overload_for<_Trest...>::get;
-        constexpr static _Tfirst get(_Tfirst);
+    template <typename _Tofirst, typename... _Torest>
+    struct _select_overload_for<_Tofirst, _Torest...> : _select_overload_for<_Torest...> {
+        using _select_overload_for<_Torest...>::get;
+        constexpr static _Tofirst get(_Tofirst);
     };
 
     // Use variant passed template arguments
@@ -113,173 +278,8 @@ private:
 
     size_t _StoredIndex;
 
-    struct _tag_first_member {};
-    struct _tag_rest_member {};
-
-    template <typename _Stor>
-    struct _storage_traits;
-
-    template <typename... _Ts>
-    struct _variant_storage {
-        constexpr _variant_storage(const _variant_storage&, size_t) {}
-        constexpr _variant_storage(_variant_storage&&, size_t) {}
-        template <typename _Ty>
-        constexpr _variant_storage(_Ty&&, size_t) {}
-    };
-
-    template <typename _Tfirst, typename... _Trest>
-    struct _variant_storage<_Tfirst, _Trest...> {
-        union {
-            _Tfirst _value_first;
-            _variant_storage<_Trest...> _storage_rest;
-        };
-
-        using _RestTraits = _storage_traits<_variant_storage<_Trest...>>;
-
-        // Default constructor
-        constexpr _variant_storage() : _value_first() {}
-
-        // Copy constructors
-        constexpr _variant_storage(_tag_first_member, const _variant_storage& o)
-            : _value_first(o._value_first) {}
-        constexpr _variant_storage(_tag_rest_member, const _variant_storage& o, size_t id)
-            : _storage_rest(o._storage_rest, id) {}
-        constexpr _variant_storage(const _variant_storage& o, size_t id)
-            : _variant_storage((id == 0)
-                ? _variant_storage(_tag_first_member(), o)
-                : _variant_storage(_tag_rest_member(), o, id-1)) {}
-
-        // Move constructors
-        constexpr _variant_storage(_tag_first_member, _variant_storage&& o)
-            : _value_first(move(o._value_first)) {}
-        constexpr _variant_storage(_tag_rest_member, _variant_storage&& o, size_t id)
-            : _storage_rest(move(o._storage_rest), id) {}
-        constexpr _variant_storage(_variant_storage&& o, size_t id)
-            : _variant_storage((id == 0)
-                ? _variant_storage(_tag_first_member(), move(o))
-                : _variant_storage(_tag_rest_member(), move(o), id-1)) {}
-        
-        // Forwarding constructors
-        template <typename _Ty>
-        constexpr _variant_storage(_tag_first_member, _Ty&& val)
-            : _value_first(forward<_Ty>(val)) {}
-        template <typename _Ty, size_t _Idx>
-        constexpr _variant_storage(_tag_rest_member, _Ty&& val, std::integral_constant<size_t, _Idx> id)
-            : _storage_rest(forward<_Ty>(val), id) {}
-        template <typename _Ty, size_t _Idx>
-        constexpr _variant_storage(_Ty&& val, std::integral_constant<size_t, _Idx> id)
-            : _variant_storage(this->_construct_index(forward<_Ty>(val), id)) {}
-        
-        template <typename _Ty, size_t _Idx>
-        constexpr static _variant_storage _construct_index(_Ty&& val, std::integral_constant<size_t, _Idx>) {
-            if constexpr (_Idx == 0)
-                return _variant_storage(_tag_first_member(), forward<_Ty>(val));
-            else
-                return _variant_storage(_tag_rest_member(), forward<_Ty>(val), std::integral_constant<size_t, _Idx-1>());
-        }
-    };
-
-
-    template <typename... _Ts>
-    struct _storage_traits<_variant_storage<_Ts...>> {
-        using Storage = _variant_storage<_Ts...>;
-        static constexpr void destroy(Storage&, size_t) {}
-        constexpr static bool equals_impl(size_t, Storage const&, Storage const&) {
-            return false;
-        }
-        constexpr static void assign_copy_impl(Storage&, Storage const&, size_t) {}
-        constexpr static void assign_move_impl(Storage&, Storage&, size_t) {}
-    };
-
-    template <typename _Tfirst, typename... _Trest>
-    struct _storage_traits<_variant_storage<_Tfirst, _Trest...>> {
-        using Storage = _variant_storage<_Tfirst, _Trest...>;
-        // Out-of-place constructor
-        constexpr static void construct_idx(Storage& l, Storage const& r, size_t id) {
-            new (&l) Storage(r, id);
-        }
-        constexpr static void construct_idx(Storage& l, Storage&& r, size_t id) {
-            new (&l) Storage(std::move(r), id);
-        }
-        template <size_t _Idx, typename _Ty>
-        constexpr static void construct_fwd(Storage& l, _Ty&& value) {
-            new (&l) Storage(std::forward<_Ty>(value), std::integral_constant<size_t, _Idx>());
-        }
-        // Destructor
-        static void destroy(Storage& s, size_t id) {
-            if (id == 0) {
-                s._value_first.~_Tfirst();
-            } else {
-                Storage::_RestTraits::destroy(s._storage_rest, id-1);
-            }
-        }
-        template <size_t _TheIdx>
-        constexpr static auto& get_impl(Storage const& storage) {
-            if constexpr (_TheIdx == 0) {
-                return storage._value_first;
-            } else {
-                return Storage::_RestTraits::template get_impl<_TheIdx-1>(storage._storage_rest);
-            }
-        }
-        constexpr static bool equals_impl(size_t id, Storage const& a, Storage const& b) {
-            if (id == 0) {
-                return a._value_first == b._value_first;
-            } else {
-                return Storage::_RestTraits::equals_impl(id-1, a._storage_rest, b._storage_rest);
-            }
-        }
-        constexpr static void assign_copy_impl(Storage& l, Storage const& r, size_t id) {
-            if (id == 0) {
-                l._value_first = r._value_first;
-            } else {
-                Storage::_RestTraits::assign_copy_impl(l._storage_rest, r._storage_rest, id-1);
-            }
-        }
-        constexpr static void assign_move_impl(Storage& l, Storage& r, size_t id) {
-            if (id == 0) {
-                l._value_first = std::move(r._value_first);
-            } else {
-                Storage::_RestTraits::assign_move_impl(l._storage_rest, r._storage_rest, id-1);
-            }
-        }
-        constexpr static void assign_copy(Storage& l, Storage const& r, size_t& il, size_t ir) {
-            if (il != ir) {
-                destroy(l, il);
-                il = ir;
-                construct_idx(l, r, ir);
-            }
-            assign_copy_impl(l, r, ir);
-        }
-        constexpr static void assign_move(Storage& l, Storage& r, size_t& il, size_t ir) {
-            if (il != ir) {
-                destroy(l, il);
-                il = ir;
-                construct_idx(l, std::move(r), ir);
-            }
-            assign_move_impl(l, r, ir);
-        }
-        template <size_t _Idx, typename _Ty>
-        constexpr static void assign_fwd_current(Storage& l, _Ty&& value) {
-            if constexpr (_Idx == 0) {
-                l._value_first = std::forward<_Ty>(value);
-            } else {
-                Storage::_RestTraits::assign_fwd_current(l._storage_rest, std::forward<_Ty>(value));
-            }
-        }
-        template <size_t _Idx, typename _Ty>
-        constexpr static void assign_fwd(Storage& l, size_t& il, _Ty&& value) {
-            if (_Idx == il) {
-                assign_fwd_current<_Idx>(l, std::forward<_Ty>(value));
-            } else {
-                destroy(l, il);
-                il = _Idx;
-                construct_fwd<_Idx>(l, std::forward<_Ty>(value));
-            }
-        }
-    };
-
     _variant_storage<_Tfirst, _Trest...> _Value;
-    using _StorageTraits = _storage_traits<_variant_storage<_Tfirst, _Trest...>>;
+    using _StorageTraits = _variant_storage_traits<_variant_storage<_Tfirst, _Trest...>>;
 public:
     template <size_t _Idx>
     constexpr variant_alternative_t<_Idx, variant>& get() {

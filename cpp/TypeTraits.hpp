65249f31737145fd8cd23917d9013f73fd18465a
CatalinNic1 <catalinnicolau2013@gmail.com>
2020-09-16 22:43:54 +0300

    Added more traits

--- cpp/TypeTraits.hpp
+++ cpp/TypeTraits.hpp
@@ -6,6 +6,89 @@
 
 namespace hsd
 {
+    struct true_type
+    {
+        static constexpr bool value = true;
+    };
+    
+    struct false_type
+    {
+        static constexpr bool value = false;
+    };
+
+    template<bool, typename>
+    struct enable_if
+    {};
+
+    template<typename T>
+    struct enable_if<true, T>
+    {
+        using type = T;
+    };
+
+    template<bool, typename>
+    struct disable_if
+    {};
+
+    template<typename T>
+    struct disable_if<false, T>
+    {
+        using type = T;
+    };
+
+    template<typename... Cond> requires(sizeof...(Cond) > 1)
+    struct conjunction
+    {
+        static constexpr bool value = (Cond::value && ...);
+    };
+
+    template<typename... Cond> requires(sizeof...(Cond) > 1)
+    struct disjunction
+    {
+        static constexpr bool value = (Cond::value || ...);
+    };
+
+    template<typename Cond>
+    struct negation
+    {
+        static constexpr bool value = !Cond::value;
+    };
+
+    template<bool Cond>
+    struct conditional
+    {
+        static constexpr bool value = Cond;
+    };
+    
+
+    template< typename, typename >
+    struct is_same 
+        : public false_type
+    {};
+    
+    template<typename T>
+    struct is_same<T, T>
+        : public true_type
+    {};
+
+    template<typename T>
+    struct remove_reference
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_reference<T&>
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_reference<T&&>
+    {
+        using type = T;
+    };
+
     template<typename T>
     struct remove_array
     {
@@ -29,12 +112,12 @@ namespace hsd
 
     template<typename>
     struct is_char_pointer 
-        : public std::false_type
+        : public false_type
     {};
 
     template<>
     struct is_char_pointer<char[]> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<usize N>
@@ -44,158 +127,158 @@ namespace hsd
 
     template<>
     struct is_char_pointer<char*> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<const char*> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<const char* const> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<wchar[]> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<usize N>
     struct is_char_pointer<wchar[N]> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<wchar*> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<const wchar*> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<const wchar* const> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<char16[]> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<usize N>
     struct is_char_pointer<char16[N]> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<char16*> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<const char16*> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<const char16* const> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<char32[]> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<usize N>
     struct is_char_pointer<char32[N]> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<char32*> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<const char32*> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char_pointer<const char32* const> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<typename>
     struct is_char
-        : public std::false_type
+        : public false_type
     {};
 
     template<>
     struct is_char<char> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char<const char> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char<uchar> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char<const uchar> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char<wchar> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char<const wchar> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char<char16> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char<const char16> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char<char32> 
-        : public std::true_type
+        : public true_type
     {};
 
     template<>
     struct is_char<const char32> 
-        : public std::true_type
+        : public true_type
     {};
 
     template< typename Condition, typename Value >
-    using ResolvedType = typename std::enable_if< Condition::value, Value >::type;
+    using ResolvedType = typename enable_if< Condition::value, Value >::type;
 
     template< typename From, typename To, typename Result >
-	using EnableIfConvertible = ResolvedType<std::is_convertible<From, To>, Result>;
+	using EnableIfConvertible = ResolvedType< std::is_convertible<From, To>, Result >;
 
     template< typename First, typename Second, typename Result >
-	using EnableIfSame = ResolvedType<std::is_same<First, Second>, Result>;
+	using EnableIfSame = ResolvedType< is_same<First, Second>, Result >;
 
     template<typename T>
-    using rev_ref_t = typename std::remove_reference<T>::type;
+    using rev_ref_t = typename remove_reference<T>::type;
 } // namespace hsd

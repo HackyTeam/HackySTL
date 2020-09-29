be880c7824de627a84bf5d3e8d0235fd3211b68c
CatalinNic1 <catalinnicolau2013@gmail.com>
2020-09-29 21:52:46 +0300

    Changed the inhertance of hsd::make_integer_sequence to not be crypitc

--- cpp/IndexSequence.hpp
+++ cpp/IndexSequence.hpp
@@ -14,16 +14,9 @@ namespace hsd
         }
     };
 
-    template <typename T, T... Next>
-    struct make_integer_sequence_helper_alternative
-    {
-        using type = integer_sequence<T, Next...>;
-    };
-
     template < typename T, T Prev, T... Next >
     struct make_integer_sequence_helper
-        : public hsd::conditional_t<Prev == 0, 
-        make_integer_sequence_helper_alternative< T, Next... >, 
+        : public hsd::conditional_t<Prev == 0, integer_sequence< T, Next... >, 
         make_integer_sequence_helper< T, Prev - 1, Prev - 1, Next... >>
     {};
 
@@ -31,10 +24,10 @@ namespace hsd
     using index_sequence = integer_sequence<usize, Next...>;
     
     template <usize N>
-    using make_index_sequence = typename make_integer_sequence_helper<usize, N>::type;
+    using make_index_sequence = make_integer_sequence_helper<usize, N>;
     
     template <typename T, T Size> requires(Size >= 0)
-    using make_integer_sequence = typename make_integer_sequence_helper<T, Size>::type;
+    using make_integer_sequence = make_integer_sequence_helper<T, Size>;
     
     template <typename... T>
     using index_sequence_for = make_index_sequence<sizeof...(T)>;

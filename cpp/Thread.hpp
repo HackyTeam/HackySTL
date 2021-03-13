0f88aedfe174af843388f1286d01b2d53bedf673
CatalinNic1 <catalinnicolau2013@gmail.com>
2021-03-13 20:34:30 +0200

    Improved hsd::thread on windows thanks to qookie

--- cpp/Thread.hpp
+++ cpp/Thread.hpp
@@ -4,75 +4,92 @@
 #include "Tuple.hpp"
 
 #if defined(HSD_PLATFORM_POSIX)
-
 #include <pthread.h>
 #include <cstdlib>
+#elif defined(HSD_PLATFORM_WINDOWS)
+#include <windows.h>
+#endif
 
 namespace hsd 
 {
 	struct thread 
 	{
+	private:
+		#if defined(HSD_PLATFORM_POSIX)
+		using native_id_type = u64;
+		#elif defined(HSD_PLATFORM_WINDOWS)
+		using native_id_type = DWORD;
+		#endif
+	public:
 		struct id 
 		{
 			friend thread;
-	
+
 			id() = default;
-	
+
 			friend bool operator==(id lhs, id rhs) 
 			{
 				return lhs._id == rhs._id;
 			}
 
 		private:
-			u64 _id;
-	
-			id(pthread_t pt) 
-				: _id{static_cast<u64>(pt)} 
+			thread::native_id_type _id;
+
+			id(thread::native_id_type id) 
+				: _id{static_cast<u64>(id)}
 			{}
 		};
-	
+
+		#if defined(HSD_PLATFORM_POSIX)
 		using native_handle_type = pthread_t;
-	
-		thread() 
-			: _id{0} 
-		{}
-	
+		#elif defined(HSD_PLATFORM_WINDOWS)
+		using native_handle_type = HANDLE;
+		#endif
+
+		thread() = default;
+
 		~thread() 
 		{
 			if (joinable())
 				abort();
-
-			detach();
 		}
-	
+
 		template <typename F, typename... Args>
 		thread(F&& func, Args&&... args) 
 		{
+			#if defined(HSD_PLATFORM_POSIX)
 			pthread_attr_t attr;
 			pthread_attr_init(&attr);
-	
+			#endif
+
 			auto decay_copy = []<typename T>(T&& t) -> decay_t<T>
 			{
 				return hsd::forward<T>(t);
 			};
-	
+
 			hsd::i32 ready = 0;
-	
+
 			struct thread_data 
 			{
 				hsd::i32* ready;
-	
+
 				hsd::decay_t<F> func;
 				hsd::tuple<decay_t<Args>...> args;
-	
+
 				static void* enter_thread(void* arg) 
 				{
 					auto td = hsd::move(*reinterpret_cast<thread_data*>(arg));
-	
+
 					// Tell our parent we are ready and copied the data
+					#if defined(HSD_PLATFORM_POSIX)
 					__atomic_store_n(td.ready, 1, __ATOMIC_RELEASE);
+					#elif defined(HSD_PLATFORM_WINDOWS)
+					InterlockedIncrement(td.ready);
+					WakeOnAddressSingle(td.ready);
+					#endif
+					
 					hsd::apply(td.func, td.args);
-	
+
 					return nullptr;
 				}
 			} td {
@@ -80,216 +97,93 @@ namespace hsd
 				decay_copy(hsd::forward<F>(func)),
 				hsd::make_tuple(decay_copy(hsd::forward<Args>(args))...)
 			};
-	
-			pthread_create(&_id, &attr, thread_data::enter_thread, &td);
-	
+
+			#if defined(HSD_PLATFORM_POSIX)
+			/*i32 err = */pthread_create(&_handle, &attr, thread_data::enter_thread, &td);
+			// if (err) raise error
+
 			// Spinlock time
 			while (!__atomic_load_n(&ready, __ATOMIC_ACQUIRE));
+			_id = id{static_cast<native_id_type>(_handle)};
+			#elif defined(HSD_PLATFORM_WINDOWS)
+			native_id_type native_id;
+			_handle = CreateThread(nullptr, 0, thread_data::enter_thread, &td, 0, &native_id);
+			// if (!_handle) raise error
+
+			i32 compare = 0;
+			WaitOnAddress(&ready, &compare, sizeof(i32), INFINITE);
+
+			_id = id{native_id};
+			#endif
 		}
-	
+
 		thread(const thread&) = delete;
 		thread& operator=(const thread&) = delete;
-	
+
 		thread(thread&& other) 
-			:_id{0} 
 		{
 			swap(other); 
 		}
-	
+
 		thread& operator=(thread&& other) 
 		{
-			swap(other); return *this; 
+			swap(other); return *this;
 		}
-	
+
 		void swap(thread &other) 
 		{ 
-			hsd::swap(_id, other._id); 
-		}
-	
-		id get_id() 
-		{
-			return id{_id};
-		}
-	
-		bool joinable() {
-			return get_id() != id{};
-		}
-	
-		native_handle_type native_handle() 
-		{
-			return _id;
-		}
-	
-		static u32 hardware_concurrency() 
-		{
-			return 0;
-		}
-	
-		void detach() 
-		{
-			pthread_detach(_id);
-			_id = 0;
-		}
-	
-		void join() 
-		{
-			pthread_join(_id, nullptr);
-		}
-	
-	private:
-		pthread_t _id;
-	}; // struct thread
-} // namespace hsd
-#else
-
-#include <windows.h>
-
-namespace hsd 
-{
-	class thread 
-	{
-	private:
-		struct thread_data_base
-		{
-			virtual ~thread_data_base() {}
-		};
-
-		DWORD _id;
-		HANDLE _handle;
-		thread_data_base* _td;
-
-	public:
-		struct id 
-		{
-			friend thread;
-	
-			id() = default;
-	
-			friend bool operator==(id lhs, id rhs) 
-			{
-				return lhs._id == rhs._id;
-			}
-		private:
-	
-			u64 _id;
-	
-			id(DWORD pt) 
-				: _id{static_cast<u64>(pt)} 
-			{}
-		};
-	
-		using native_handle_type = HANDLE;
-	
-		thread() 
-			: _id{0}, _handle{nullptr}, _td{nullptr}
-		{}
-	
-		~thread() 
-		{
-			if (joinable())
-				abort();
-
-			detach();
-		}
-	
-		template <typename F, typename... Args>
-		thread(F&& func, Args&&... args) 
-		{
-			auto decay_copy = []<typename T>(T&& t) -> decay_t<T>
-			{
-				return hsd::forward<T>(t);
-			};
-	
-			struct thread_data 
-				: public thread_data_base
-			{
-				hsd::decay_t<F> func;
-				hsd::tuple<decay_t<Args>...> args;
-
-				using func_type = hsd::decay_t<F>;
-				using args_type = hsd::tuple<decay_t<Args>...>;
-	
-				thread_data(func_type&& _func, args_type&& _args)
-					: func{forward<func_type>(_func)}, 
-					args{forward<args_type>(_args)}
-				{}
-
-				~thread_data() = default;
-
-				static DWORD enter_thread(void* arg) 
-				{
-					auto* td = reinterpret_cast<thread_data*>(arg);
-					hsd::apply(td->func, td->args);
-	
-					return 0ul;
-				}
-			};
-	
-			_td = new thread_data{
-				decay_copy(hsd::forward<F>(func)),
-				hsd::make_tuple(decay_copy(hsd::forward<Args>(args))...)
-			};
-
-			_handle = CreateThread(
-				nullptr, 0, thread_data::enter_thread, _td, 0, &_id
-			); 
-		}
-	
-		thread(const thread&) = delete;
-		thread& operator=(const thread&) = delete;
-	
-		thread(thread&& other) 
-			: _id{0}, _handle{nullptr}, _td{nullptr}
-		{
-			swap(other);
-		}
-	
-		thread& operator=(thread&& other) 
-		{
-			swap(other); return *this; 
-		}
-	
-		void swap(thread& other) 
-		{ 
+			hsd::swap(_handle, other._handle);
 			hsd::swap(_id, other._id);
-			hsd::swap(_td, other._td);
-			hsd::swap(_handle, other._handle); 
 		}
-	
+
 		id get_id() 
 		{
 			return id{_id};
 		}
-	
+
 		bool joinable() {
 			return get_id() != id{};
 		}
-	
+
 		native_handle_type native_handle() 
 		{
 			return _handle;
 		}
-	
+
 		static u32 hardware_concurrency() 
 		{
 			return 0;
 		}
-	
+
 		void detach() 
 		{
+			#if defined(HSD_PLATFORM_POSIX)
+			pthread_detach(_handle);
+			_handle = 0;
+			#elif defined(HSD_PLATFORM_WINDOWS)
 			CloseHandle(_handle);
-			delete _td;
-
-			_id = 0;
 			_handle = nullptr;
-			_td = nullptr;
+			#endif
+			
+			_id = {};
 		}
-	
+
 		void join() 
 		{
+			#if defined(HSD_PLATFORM_POSIX)
+			pthread_join(_handle, nullptr);
+			_handle = 0;
+			#elif defined(HSD_PLATFORM_WINDOWS)
 			WaitForSingleObject(_handle, INFINITE);
-			_id = 0;
+			CloseHandle(_handle);
+			_handle = nullptr;
+			#endif
+
+			_id = {};
 		}
+	
+	private:
+		native_handle_type _handle { };
+		id _id { };
 	}; // struct thread
 } // namespace hsd
-#endif

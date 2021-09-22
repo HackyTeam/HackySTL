35749769b459ad591eed0cf03ca8584b1c2c42b5
Anonymus-Player <AnonymusPlayer@protomail.com>
2021-09-22 19:19:18 +0300

    Removed semicolum found at the end of variant constructor

--- Tests/Io/IoTest.cpp
+++ Tests/Io/IoTest.cpp
@@ -5,11 +5,11 @@ int main()
     hsd::wsstream str{256};
     hsd::i32 x, z;
     hsd::f32 y;
-    hsd_println(L"hello, {e,fg=200,bg=62} and other words", 123.2);
+    hsd_println(L"hello, {} and other words", 123.2);
     str.write_data<L"hello, {} and other words\n">(hsd::string{"123.2"});
     
     hsd::io::cin().read_line().unwrap().set_data(x, y, z).unwrap();
-    hsd_println(L"{}, {}, {}", x, y, z);
+    hsd_println(L"{x,fg=234,bg=43}, {e,fg=143,bg=78}, {fg=84,bg=193}", x, y, z);
     
     auto file = hsd::io::load_file(
         "Tests/Io/test.txt", hsd::io::options::text::read_write

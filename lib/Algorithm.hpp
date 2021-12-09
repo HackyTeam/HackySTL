0153e01acb4c4a0d9b259a52c27ef2936e88712b
Anonymus-Player <catalinnicolau2013@gmail.com>
2021-12-09 13:21:55 +0200

    Added non-const overload for hsd::for_each

--- lib/Algorithm.hpp
+++ lib/Algorithm.hpp
@@ -142,4 +142,12 @@ namespace hsd
             func(_iter);
         }
     }
+
+    static constexpr void for_each(auto& arr, auto&& func)
+    {
+        for (const auto& _iter : arr)
+        {
+            func(_iter);
+        }
+    }
 } // namespace hsd
